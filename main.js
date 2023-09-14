24363a276112f5ef53f3de786764cd2b17f37e08
Abdelrahman Eid <143188634+Abdelrahman842003@users.noreply.github.com>
2023-09-14 21:58:11 +0200

    phone (HTML & CSS & js)

---
+++ main.js
@@ -0,0 +1,62 @@
+let image = document.querySelector(".image");
+let container = document.querySelector(".container")
+let imagePhone1 = document.querySelector(".imagePhone1")
+let imagePhone2 = document.querySelector(".imagePhone2")
+let imagePhone3 = document.querySelector(".imagePhone3")
+let imagePhone4 = document.querySelector(".imagePhone4")
+let imagePhone5 = document.querySelector(".imagePhone5")
+let chick = true;
+
+function changeColorPhone(src) {
+    image.src = src;
+}
+
+function changeColorBackground(color) {
+    container.style.background = color
+}
+
+if (color1 == onclick) {
+    function color1() {
+        imagePhone1.style.cssText = "opacity:1; transition:.4s ease all; transform: scale(1.2);";
+        imagePhone2.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone3.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone4.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone5.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+    }
+}
+if (color2 == onclick) {
+    function color2() {
+        imagePhone2.style.cssText = "opacity:1; transition:.4s ease all; transform: scale(1.2);";
+        imagePhone1.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone3.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone4.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone5.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+    }
+}
+if (color3 == onclick) {
+    function color3() {
+        imagePhone3.style.cssText = "opacity:1; transition:.4s ease all; transform: scale(1.2);";
+        imagePhone2.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone1.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone4.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone5.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+    }
+}
+if (color4 == onclick) {
+    function color4() {
+        imagePhone4.style.cssText = "opacity:1; transition:.4s ease all; transform: scale(1.2);";
+        imagePhone2.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone3.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone1.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone5.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+    }
+}
+if (color5 == onclick) {
+    function color5() {
+        imagePhone5.style.cssText = "opacity:1; transition:.4s ease all; transform: scale(1.2);";
+        imagePhone2.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone3.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone4.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+        imagePhone1.style.cssText = "opacity:.3; transition:.4s ease all; transform: scale(1);";
+    }
+}
